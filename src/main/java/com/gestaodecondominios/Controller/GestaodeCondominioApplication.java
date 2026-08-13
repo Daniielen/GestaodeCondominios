d8304a427de24c083f9684e93e3f2eb4ed44c6b0
daniielen <danielensantana14@gmail.com>
2026-08-13 20:35:42 -0300

    First Commit :tada::sparkles:

---
+++ src/main/java/com/gestaodecondominios/Controller/GestaodeCondominioApplication.java
@@ -0,0 +1,13 @@
+package com.gestaodecondominios.Controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaodeCondominioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaodeCondominioApplication.class, args);
+	}
+
+}
