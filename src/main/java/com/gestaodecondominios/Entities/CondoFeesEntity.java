abf3db0d1aad569ec9301f891f384eaad84c7f8e
Lorran <18370377+lorrandavi@users.noreply.github.com>
2026-08-14 12:28:08 -0300

    Implemented CondoFeesEntity

--- src/main/java/com/gestaodecondominios/Entities/CondoFeesEntity.java
+++ src/main/java/com/gestaodecondominios/Entities/CondoFeesEntity.java
@@ -1,5 +1,79 @@
 package com.gestaodecondominios.Entities;
 
-public class CondoFeesEntity {
 
+import java.time.LocalDate;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_CondoFees")
+
+public class CondoFeesEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long Id;
+	
+	@ManyToOne
+	@JoinColumn(name = "unit_id")
+	private Long unitId;
+	
+	@Column(nullable = false)
+	private float amount;
+	
+	@Column(nullable = false)
+	private LocalDate dueDate;
+	
+	@Column(nullable = false)
+	private String status;
+	
+	@Column(nullable = false)
+	private LocalDate paymentDate;
+
+	public Long getId() {
+		return Id;
+	}
+
+	public void setId(Long id) {
+		Id = id;
+	}
+
+	public Long getUnitId() {
+		return unitId;
+	}
+
+	public void setUnitId(Long unitId) {
+		this.unitId = unitId;
+	}
+
+	public float getAmount() {
+		return amount;
+	}
+
+	public void setAmount(float amount) {
+		this.amount = amount;
+	}
+
+	public LocalDate getDueDate() {
+		return dueDate;
+	}
+
+	public void setDueDate(LocalDate dueDate) {
+		this.dueDate = dueDate;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public LocalDate getPaymentDate() {
+		return paymentDate;
+	}
+
+	public void setPaymentDate(LocalDate paymentDate) {
+		this.paymentDate = paymentDate;
+	}
 }
