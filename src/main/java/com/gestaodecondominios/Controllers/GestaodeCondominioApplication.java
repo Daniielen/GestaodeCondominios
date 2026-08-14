9b48dcc7de1d059d3127c6e25600499004dc186c
daniielen <danielensantana14@gmail.com>
2026-08-13 21:47:44 -0300

    :sparkles:

--- src/main/java/com/gestaodecondominios/Controllers/GestaodeCondominioApplication.java
+++ src/main/java/com/gestaodecondominios/Controllers/GestaodeCondominioApplication.java
@@ -1,4 +1,4 @@
-package com.gestaodecondominios;
+package com.gestaodecondominios.Controllers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
